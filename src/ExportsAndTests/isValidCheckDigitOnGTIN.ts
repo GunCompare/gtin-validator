8fb89e963caf8e6e22b972e4826b838a34800ea1
DoUWant2DevApp <93307121+DoUWant2DevApp@users.noreply.github.com>
2022-10-08 22:04:16 -0400

    make params in example strings as they should be.

--- src/ExportsAndTests/isValidCheckDigitOnGTIN.ts
+++ src/ExportsAndTests/isValidCheckDigitOnGTIN.ts
@@ -6,19 +6,19 @@ import { isValidStringLengthForGTIN } from "./isValidStringLengthForGTIN"
  * @param productCode 8 or 12-14 digit GTIN including check digit
  * @param [returnBooleanOrThrowError] default = 'boolean'
  * @returns true if productCode is a valid GTIN with valid check digit.
- * @example isValidCheckDigitOnGTIN(078000053401) //returns true
- * isValidCheckDigitOnGTIN(078000053401, "boolean") //returns true
- * isValidCheckDigitOnGTIN(078000053401, "error") //returns true
- * isValidCheckDigitOnGTIN(01012345678905) //returns false
- * isValidCheckDigitOnGTIN(01012345678905, "boolean") //returns false
- * isValidCheckDigitOnGTIN(01012345678905, "error") //throws error "Invalid GTIN: GTIN check digit is invalid."
+ * @example isValidCheckDigitOnGTIN("078000053401") //returns true
+ * isValidCheckDigitOnGTIN("078000053401", "boolean") //returns true
+ * isValidCheckDigitOnGTIN("078000053401", "error") //returns true
+ * isValidCheckDigitOnGTIN("01012345678905") //returns false
+ * isValidCheckDigitOnGTIN("01012345678905", "boolean") //returns false
+ * isValidCheckDigitOnGTIN("01012345678905", "error") //throws error "Invalid GTIN: GTIN check digit is invalid."
  * isValidCheckDigitOnGTIN("","error") //throws error "Invalid GTIN: The provided productCode is an empty string."
  * isValidCheckDigitOnGTIN("84656","error") //throws error "Invalid GTIN: Valid GTINs must 8 digits or 12-14 digits."
- * isValidCheckDigitOnGTIN(722510035005, "error") //returns true
- * isValidCheckDigitOnGTIN(722510035003, "error") //throws error "Invalid GTIN: GTIN check digit is invalid.""
- * isValidCheckDigitOnGTIN(722510035003, "boolean") //returns false
- * isValidCheckDigitOnGTIN(722510035003) //returns false
- * isValidCheckDigitOnGTIN(00012345678905) //returns true
+ * isValidCheckDigitOnGTIN("722510035005", "error") //returns true
+ * isValidCheckDigitOnGTIN("722510035003", "error") //throws error "Invalid GTIN: GTIN check digit is invalid.""
+ * isValidCheckDigitOnGTIN("722510035003", "boolean") //returns false
+ * isValidCheckDigitOnGTIN("722510035003") //returns false
+ * isValidCheckDigitOnGTIN("00012345678905") //returns true
  */
 
 export function isValidCheckDigitOnGTIN(
